aeb49989121248776755f90d33d8ef847ac37414
martengeets <64744860+martengeets@users.noreply.github.com>
2020-10-09 16:47:30 +0200

    ball trajectory

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public Vector3 impulse = new Vector3(0.0f, 0.0f, 0.0f);
+    bool shootButtonWasPressed = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shootButtonWasPressed = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (shootButtonWasPressed)
+        {
+            GetComponent<Rigidbody>().AddForce(impulse, ForceMode.Impulse);
+            shootButtonWasPressed = false;
+        }
     }
 }
