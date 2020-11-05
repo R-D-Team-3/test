bf30c573adc5b52de9c06f58034f3dbb336b2363
W-Axel <49104234+W-Axel@users.noreply.github.com>
2020-11-05 20:35:49 +0100

    Camera update MP
    + MainCamera now attaches to Player
    (Disable Player prefab child camera)

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -13,7 +13,20 @@ public class PlayerManager : MonoBehaviourPun
     // Start is called before the first frame update
     void Start()
     {
-        
+        CameraWork _cameraWork = this.gameObject.GetComponent<CameraWork>();
+
+
+        if (_cameraWork != null)
+        {
+            if (photonView.IsMine)
+            {
+                _cameraWork.OnStartFollowing();
+            }
+        }
+        else
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
+        }
     }
 
     private void Awake()
